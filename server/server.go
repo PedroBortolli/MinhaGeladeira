3000783126c0981598d66233c25851f8dbf9a29d
RAKTanaka <raktanaka@gmail.com>
2019-10-07 16:07:39 -0300

    Added folder containing test images, the Go API now fetches text

--- server/server.go
+++ server/server.go
@@ -1,8 +1,10 @@
 package main
 
+// Inicializar ambiente
+// export GOOGLE_APPLICATION_CREDENTIALS="...googlekey.json"
+
 import (
 	"context"
-	"fmt"
 	"log"
 	"os"
 
@@ -20,7 +22,7 @@ func main() {
 	defer client.Close()
 
 	// Sets the name of the image file to annotate.
-	filename := "../teste.jpg"
+	filename := "../testfiles/teste1.jpg"
 
 	file, err := os.Open(filename)
 	if err != nil {
@@ -32,13 +34,51 @@ func main() {
 		log.Fatalf("Failed to create image: %v", err)
 	}
 
-	labels, err := client.DetectLabels(ctx, image, nil, 10)
+	text, err := client.DetectTexts(ctx, image, nil, 10)
 	if err != nil {
-		log.Fatalf("Failed to detect labels: %v", err)
+		log.Fatalf("Failed to detect text: %v", err)
 	}
 
-	fmt.Println("Labels:")
-	for _, label := range labels {
-		fmt.Println(label.Description)
-	}
+	log.Printf("Extracted text %q from image (%d chars).", text, len(text))
+
 }
+
+/*
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"cloud.google.com/go/pubsub"
+	"golang.org/x/text/language"
+	visionpb "google.golang.org/genproto/googleapis/cloud/vision/v1"
+)
+
+// detectText detects the text in an image using the Google Vision API.
+func detectText(ctx context.Context, bucketName) error {
+	filename := "../testfiles/teste1.jpg"
+	log.Printf("Looking for text in image %v", fileName)
+	maxResults := 1
+	image := &visionpb.Image{
+		Source: &visionpb.ImageSource{
+			GcsImageUri: fmt.Sprintf("gs://%s/%s", bucketName, fileName),
+		},
+	}
+	annotations, err := visionClient.DetectTexts(ctx, image, &visionpb.ImageContext{}, maxResults)
+	if err != nil {
+		return fmt.Errorf("DetectTexts: %v", err)
+	}
+	text := ""
+	if len(annotations) > 0 {
+		text = annotations[0].Description
+	}
+	if len(annotations) == 0 || len(text) == 0 {
+		log.Printf("No text detected in image %q. Returning early.", fileName)
+		return nil
+	}
+	log.Printf("Extracted text %q from image (%d chars).", text, len(text))
+
+	return nil
+}
+*/
